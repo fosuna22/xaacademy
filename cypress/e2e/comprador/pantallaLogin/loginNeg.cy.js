3ee93d6fec41887982eb88c69d6ef7e816b717f4
fosuna22 <francoosuna2202@gmail.com>
2025-09-04 18:23:20 -0300

    aaa

--- cypress/e2e/comprador/pantallaLogin/loginNeg.cy.js
+++ cypress/e2e/comprador/pantallaLogin/loginNeg.cy.js
@@ -1,5 +1,5 @@
 describe('TC004 - Login de usuario comprador casos negativos', () => {
-    it.only('No permite login con email incorrecto', () => {
+    it('No permite login con email incorrecto', () => {
         cy.loginCompradorEmailNeg();
     })
     it('No permite login con contraseña incorrecta', () => {
