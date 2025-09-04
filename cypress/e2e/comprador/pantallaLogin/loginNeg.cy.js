8e0913b44f350bbbce6ca16a75afec6b4e756482
fosuna22 <francoosuna2202@gmail.com>
2025-09-04 18:19:06 -0300

    Se actualizó el test de login negativo para compradores, marcando el caso de email incorrecto como 'only' para su ejecución exclusiva durante las pruebas.

--- cypress/e2e/comprador/pantallaLogin/loginNeg.cy.js
+++ cypress/e2e/comprador/pantallaLogin/loginNeg.cy.js
@@ -1,5 +1,5 @@
 describe('TC004 - Login de usuario comprador casos negativos', () => {
-    it('No permite login con email incorrecto', () => {
+    it.only('No permite login con email incorrecto', () => {
         cy.loginCompradorEmailNeg();
     })
     it('No permite login con contraseña incorrecta', () => {
